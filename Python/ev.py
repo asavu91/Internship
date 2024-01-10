594f301d23cda265b6e574fac9169faed6b5d779
asavu91 <152881093+asavu91@users.noreply.github.com>
2024-01-10 13:43:09 +0200

    Added ev.py

---
+++ Python/ev.py
@@ -0,0 +1,15 @@
+def calculate_range():
+    battery_cap = 62
+    energy_consumption = 15.6
+    battery_level_input = int(input("Insert current battery level:"))
+
+    if battery_level_input < battery_cap or battery_level_input == battery_cap:
+        battery_level = (100 * battery_level_input) / 62
+        procentage_battery_level = (battery_cap * battery_level) / 100
+        estimated_range = round((procentage_battery_level / energy_consumption) * 100)
+        print(str(estimated_range) + " Km")
+    else:
+        print("Please insert a number between 0 and 62")
+
+
+calculate_range()
