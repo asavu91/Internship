82ff74e2e08946438b608315ec40073580a8b47f
Savu Adrian <adrian.savu@randstaddigital.com>
2024-02-08 12:05:26 +0200

    now prints Species [species], Type [type]

--- Automation/API/species_list.py
+++ Automation/API/species_list.py
@@ -3,10 +3,10 @@ import requests
 episode_ids = [22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 api_episode = "https://rickandmortyapi.com/api/episode/"
 
-species_set = set()
+species_list = []
 
 for episode_id in episode_ids:
-    episode_url = f"{api_episode}/{episode_id}"
+    episode_url = f"{api_episode}{episode_id}"
     response = requests.get(episode_url)
     episode_data = response.json()
     characters = episode_data.get("characters", [])
@@ -17,10 +17,9 @@ for episode_id in episode_ids:
         species = character_data.get("species")
         character_type = character_data.get("type")
 
-        if species != "unknown" and character_type != "":
-            species_set.add((species, character_type))
+        if species and character_type:
+            species_list.append({"species": species, "type": character_type})
 
 print("Species types that appear in Season 3:")
-for species, character_type in species_set:
-    if species != "Human":
-        print(f"{species}: {character_type}")
+for species_data in species_list:
+    print(f'Species: {species_data["species"]}, Type: {species_data["type"]}')
